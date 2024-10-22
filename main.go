2bd4e294c38afb808ddf49a29eccc5e08b76bea2
Abdur Roquib Pramanik <roquib01@outlook.com>
2024-10-22 16:09:02 +0600

    Initial Commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	// MongoDB connection URI
+	uri := "mongodb://localhost:27017"
+
+	// Set up client options
+	clientOptions := options.Client().ApplyURI(uri)
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal("Failed to connect to MongoDB:", err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+}
